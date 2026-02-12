233a1d42f2cb913e85dbaee904afcb447adc3e68
KennOliver <kennoliverf@gmail.com>
2026-02-12 17:43:42 +0100

    Estructura inicial del sensor

---
+++ air-sensor/src/main/java/com/tufm/air_sensor/AirSensorApplication.java
@@ -0,0 +1,13 @@
+package com.tufm.air_sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirSensorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirSensorApplication.class, args);
+	}
+
+}
---
+++ air-sensor/src/test/java/com/tufm/air_sensor/AirSensorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tufm.air_sensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirSensorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
