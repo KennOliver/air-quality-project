b4ca91f7cd1cf36856bc120d934d10c8eabbe8a3
KennOliver <kennoliverf@gmail.com>
2026-02-13 14:43:00 +0100

    Refactor: Extraer valores 'hardcodeados' a application.properties

--- air-sensor/src/main/java/com/tufm/air_sensor/service/AirSensorService.java
+++ air-sensor/src/main/java/com/tufm/air_sensor/service/AirSensorService.java
@@ -1,5 +1,6 @@
 package com.tufm.air_sensor.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -8,41 +9,56 @@ import java.util.Random;
 
 @Service
 public class AirSensorService {
-    private final Random random = new Random();
 
+    // --- VARIABLES DE CONFIGURACIÓN  ---
+
+    @Value("${sensor.probabilidad-critica}")
+    private double probabilidadCritica;
+
+    @Value("${sensor.temp.normal.base}")
+    private double tempNormalBase;
+
+    @Value("${sensor.temp.normal.rango}")
+    private double tempNormalRango;
+
+    @Value("${sensor.temp.critica.base}")
+    private double tempCriticaBase;
+
+    @Value("${sensor.temp.critica.rango}")
+    private double tempCriticaRango;
+
+    // --- HERRAMIENTAS ---
+    private final Random random = new Random();
     private final KafkaTemplate<String, String> kafkaTemplate;
 
-    // Constructor para que Spring nos de el KafkaTemplate listo para usar
+    // Constructor
     public AirSensorService(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
+
     @Scheduled(fixedRate = 5000)
     public void simularMedicion() {
 
-            double probabilidad = random.nextDouble();
+        double probabilidad = random.nextDouble();
+        double temperatura;
+        double co2;
 
-            double temperatura;
-            double co2;
+        if (probabilidad < probabilidadCritica) {
+            // Caso NORMAL
+            temperatura = tempNormalBase + (tempNormalRango * random.nextDouble());
+            co2 = 400 + (400 * random.nextDouble());
+        } else {
+            // Caso CRITICO
+            temperatura = tempCriticaBase + (tempCriticaRango * random.nextDouble());
 
-            if (probabilidad < 0.9) {
+            co2 = 1000 + (1000 * random.nextDouble());
 
-                temperatura = 20 + (5 * random.nextDouble());
-                // CO2 entre 400 y 800 ppm (partes por millón)
-                co2 = 400 + (400 * random.nextDouble());
-
-            } else {
-                // 10% de las veces: VALORES CRÍTICOS (Incendio o fallo)
-                // Temp disparada entre 35 y 50 grados
-                temperatura = 35 + (15 * random.nextDouble());
-                // CO2 peligroso entre 1000 y 2000 ppm
-                co2 = 1000 + (1000 * random.nextDouble());
-
-                System.out.println("⚠️ ALERTA: VALORES CRÍTICOS DETECTADOS");
-            }
-        String mensaje = String.format("SensorID: 1 | Temp: %.2f | CO2: %.2f", temperatura, co2);
-        kafkaTemplate.send("mediciones-aire", mensaje);
-        System.out.println("Mensaje enviado a Kafka: " + mensaje);
-            System.out.printf("Sensor activo -> Temp: %.2f ºC | CO2: %.2f ppm%n", temperatura, co2);
+            System.out.println("⚠️ ALERTA: VALORES CRÍTICOS DETECTADOS");
         }
 
+        String mensaje = String.format("SensorID: 1 | Temp: %.2f | CO2: %.2f", temperatura, co2);
+        kafkaTemplate.send("mediciones-aire", mensaje);
+
+        System.out.println("Mensaje enviado a Kafka: " + mensaje);
+    }
 }
